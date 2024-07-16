1272db65c54c302466eddfe86a693f701d402261
anoopshetty03 <148759181+anoopshetty03@users.noreply.github.com>
2024-07-16 20:17:41 +0530

    Add files via upload

---
+++ Arrays/Extras/RotateArray(Left&Right).cpp
@@ -0,0 +1,35 @@
+void Reverse(int arr[], int start, int end)
+{
+  while (start <= end)
+  {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+
+// Rotate Rotate k elements to right
+void Rotateeletoright(int arr[], int n, int k)
+{
+  // Reverse first n-k elements
+  Reverse(arr, 0, n - k - 1);
+  // Reverse last k elements
+  Reverse(arr, n - k, n - 1);
+  // Reverse whole array
+  Reverse(arr, 0, n - 1);
+}
+
+
+// Rotate k elements to left
+void Rotateeletoleft(int arr[], int n, int k)
+{
+  // Reverse first k elements
+  Reverse(arr, 0, k - 1);
+  // Reverse last n-k elements
+  Reverse(arr, k, n - 1);
+  // Reverse whole array
+  Reverse(arr, 0, n - 1);
+}
+
